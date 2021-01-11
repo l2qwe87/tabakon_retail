111242012eb3f00cd6a3981c567a1f67a3a19d15
l2qwe87@gmail.com <l2qwe87@gmail.com>
2021-01-11 10:10:50 +0300

    RESET TO Alpha_2020-12-31

---
+++ src/tabakon.net/Tabakon.DALMigration/Migrations/20201223002326_RetailGetStoreBalance.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tabakon.DALMigration.Migrations
+{
+    public partial class RetailGetStoreBalance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RetailGetStoreBalance",
+                columns: table => new
+                {
+                    RetailEndpointIdentity = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    LastCheck = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    JsonData = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RetailGetStoreBalance", x => x.RetailEndpointIdentity);
+                    table.ForeignKey(
+                        name: "FK_RetailGetStoreBalance_RetailEndpoint_RetailEndpointIdentity",
+                        column: x => x.RetailEndpointIdentity,
+                        principalTable: "RetailEndpoint",
+                        principalColumn: "RetailEndpointIdentity",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RetailGetStoreBalance");
+        }
+    }
+}
