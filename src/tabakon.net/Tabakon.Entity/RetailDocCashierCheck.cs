a9fde833198925be4dbc2dff44fc1d293ebbe12f
l2qwe87@gmail.com <l2qwe87@gmail.com>
2022-09-12 01:03:19 +0300

    CashierCheck_Sum_StoreRef

--- src/tabakon.net/Tabakon.Entity/RetailDocCashierCheck.cs
+++ src/tabakon.net/Tabakon.Entity/RetailDocCashierCheck.cs
@@ -6,6 +6,8 @@ namespace Tabakon.Entity
 {
     public class RetailDocCashierCheck : AbstractDocEntity
     {
+        public decimal Sum { get; set; }
+        public string StoreRef { get; set; }
         public RetailDocCashierCheck() : base()
         {
             DocType = DocType.CashierCheck;
@@ -16,6 +18,8 @@ namespace Tabakon.Entity
 
             var jObject = JObject.Parse(json);
             this.DocRef = jObject.Value<string>("CashierCheckReportRef");
+            this.StoreRef = jObject.Value<string>("StoreRef");
+            this.Sum = jObject.Value<decimal>("CashierCheckReportSum");
             try
             {
                 this.DocDate = DateTime.ParseExact(jObject.Value<string>("CashierCheckReportDate"), "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
